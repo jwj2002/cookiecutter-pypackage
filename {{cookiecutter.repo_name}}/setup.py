7ad053811ef503bfddb993f4c0499756f807d675
Jason Job <jasonwadejob@gmail.com>
2019-03-26 21:41:03 -0700

    Added setup.py.

---
+++ {{cookiecutter.repo_name}}/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+setup(
+    name='{{ cookiecutter.repo_name }}',
+    version='{{ cookiecutter.version }}',
+    description='{{ cookiecutter.project_short_description }}',
+    author='{{ cookiecutter.full_name }}',
+    author_email='{{ cookiecustter.email }}',
+    url='https://github/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
+    packages=[
+        '{{ cookiecutter.repo_name }}',
+    ],
+    package_dir={'{{ cookiecutter.repo_name }}': '{{ cookiecutter.repo_name }}'},
+    include_package_date=True,
+    install_requires=[
+    ],
+    license='MIT',
+    zip_safe=False,
+    keywords='{{ cookiecutter.repo_name }}',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+    #    'Programming Language :: Python :: 2',
+    #    'Programming Language :: Python :: 2.6',
+    #    'Programming Language :: Python :: 2.7',
+    #    'Programming Language :: Python :: 3',
+    #    'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.7',
+    #    'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
