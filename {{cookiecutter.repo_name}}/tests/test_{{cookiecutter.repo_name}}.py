2600e67e95b93884e3f0a96d705435545cfea564
Jason Job <jasonwadejob@gmail.com>
2019-03-26 21:09:56 -0700

    Added {{cookiecutter.repo_name}}

---
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.repo_name}}.py
@@ -0,0 +1,12 @@
+"""
+Tests for `{{ cookiecutter.repo_name }}` module.
+"""
+
+import pytest
+
+from {{ cookiecutter.repo_name }} import {{ cookiecutter.repo_name }}
+
+class Test_{{ cookiecutter.repo_name|capitalize }}(object):
+
+    def test_something(self):
+        pass
